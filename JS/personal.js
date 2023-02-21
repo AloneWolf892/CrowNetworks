7b75a8969dcaef9c1ee6ac5ed22d46cba19d458f
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2023-02-21 14:36:03 +0100

    oupdate pivipi

--- JS/personal.js
+++ JS/personal.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#github-link").on("click", function(){
         window.open("https://github.com/AloneWolf892", "_newtab");
     })
+    $("#twitter-link").on("click", function(){
+        window.open("https://twitter.com/AloneWolf892", "_newtab")
+    })
 })
