96a39a948362c57d70ccfd9699d441a0c78fbe4e
AloneWolf892 <60964787+AloneWolf892@users.noreply.github.com>
2023-02-21 14:27:59 +0100

    update

---
+++ JS/personal.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+        $(document).ready(function(){
+            $("#github-link").on("click", function(){
+                window.location.href = "https://github.com/AloneWolf892"
+            })
+        })
+</script>
