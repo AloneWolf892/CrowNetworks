9ca2ff603c5c84d78ff10e6a10b7ecc27cbc3460
Cesar Bello <cesarkuroshiro@hotmail.com>
2024-04-04 10:24:45 +0000

    Changed to CDN and added passgen

--- JS/personal.js
+++ JS/personal.js
@@ -1,8 +1,31 @@
-$(document).ready(function(){
-    $("#github-link").on("click", function(){
+$(document).ready(() => {
+    const UpperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const LowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
+    const Numbers = "0123456789"
+
+    $("#github-link").on("click", () => {
         window.open("https://github.com/AloneWolf892", "_newtab");
     })
-    $("#twitter-link").on("click", function(){
+    $("#twitter-link").on("click", () => {
         window.open("https://twitter.com/AloneWolf892", "_newtab")
     })
+    $("#password-input").on("click", () => {
+        const length = 8
+        let password = ""
+
+        for(let position = 0; position < length; position++) {
+           if (position === 0) {
+               const random = UpperCaseChars.charAt(Math.floor(Math.random() * UpperCaseChars.length))
+               password += random
+           } else if (position >= 1 && position <= 2) {
+               const random = LowerCaseChars.charAt(Math.floor(Math.random() * LowerCaseChars.length))
+               password += random
+           } else if (position > 2) {
+               const random = Numbers.charAt(Math.floor(Math.random() * Numbers.length))
+               password += random
+           }
+        }
+        $("#password-output").val(password)
+    })
 })
+
